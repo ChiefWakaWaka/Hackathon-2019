d7f876579e78685e6a26c7634ff60acd91c4b1cf
Walker Davis <walker@wkeithdavis.com>
2019-01-26 20:28:19 -0500

    testing static page

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def hello():
-    return "Hello World!"
+from flask import Flask, request, send_from_directory
+app = Flask(__name__, static_url_path='/index.html')
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('js', path)
+
+if __name__ == "__main__":
+    app.run()
 
 #RUN IN TERMINAL:
 #   export FLASK_APP=hello.py
