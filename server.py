8400e607eb0f191d91f82e355ba06d2737963366
Walker Davis <walker@wkeithdavis.com>
2019-01-27 10:18:55 -0500

    server fixes

--- server.py
+++ server.py
@@ -7,25 +7,23 @@ processedText = []
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-
 @app.route("/")
 def index():
         global processedText
-        return render_template('index.html', question=processedT$
+        return render_template('index.html', question=processedText)
 
 
 @app.route('/', methods=['POST'])
-
 def post():
         global processedText
         text = request.form['text']
         processedText = [text] + processedText
         re = True
-        return render_template('index.html', question=processedT$
+        return render_template('index.html', question=processedText)
 
 
 if __name__ == '__main__':#
-        app.run(host='10.0.0.135', port=80)
-        if re = True:
+        app.run(host='127.0.0.1', port=8000)
+        if re == True:
                 app.run()
                 re = False
