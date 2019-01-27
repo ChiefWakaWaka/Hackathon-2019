b427fa9df6ba276c4fce1e74185cd4f36f581189
Walker Davis <walker@wkeithdavis.com>
2019-01-26 22:26:39 -0500

    Figured out how to link multiple pages

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-@app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/test1")
+def test():
+    return render_template('test1.html')
+
+@app.route("/test2")
+def test2():
+    return render_template('test2.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1')
+    app.run(debug=True, host='10.0.0.106')
