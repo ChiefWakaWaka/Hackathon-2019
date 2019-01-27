f1a59a7c9e88a9a32bf1d58aa7427a4bfbd45fa3
Sami <samimac03@gmail.com>
2019-01-26 20:31:36 -0500

    Walker is a nerd

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template('index.html')
 
 #RUN IN TERMINAL:
 #   export FLASK_APP=hello.py
 #   flask run
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1')
